c15a81d235df603e1bcdc6e8633ea4da42c345c6
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-03-04 16:41:56 +0800

    Add MyBanks, Transfer, and TransactionHistory pages; update RootLayout for hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}${ibmPlexSerif.variable}`}>
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable}`}
+        suppressHydrationWarning
+      >
         {children}
       </body>
     </html>
