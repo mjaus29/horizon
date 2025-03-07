1f0b3aa4048c06a7d4b22b3f6c670aace0241bae
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-03-07 13:15:28 +0800

    Add Dwolla integration; update AuthForm to require all sign-up fields and implement PlaidLink component enhancements

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,29 +1,58 @@
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { PlaidLinkOptions } from "react-plaid-link";
+import {
+  PlaidLinkOptions,
+  PlaidLinkOnSuccess,
+  usePlaidLink,
+} from "react-plaid-link";
+import { useRouter } from "next/navigation";
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from "@/lib/actions/user.actions";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
   const [token, setToken] = useState("");
 
   useEffect(() => {
     const getLinkToken = async () => {
-      //   const data = await createLinkToken(user);
+      const data = await createLinkToken(user);
+      setToken(data?.linkToken);
     };
 
     getLinkToken();
-  }, []);
+  }, [user]);
 
-  const onSuccess = useCallback(async () => {}, [user]);
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      });
+
+      router.push("/");
+    },
+    [user]
+  );
 
   const config: PlaidLinkOptions = {
     token,
     onSuccess,
   };
 
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary">Connect bank</Button>
+        <Button
+          className="plaidlink-primary"
+          onClick={() => open()}
+          disabled={!ready}
+        >
+          Connect bank
+        </Button>
       ) : variant === "ghost" ? (
         <Button>Connect bank</Button>
       ) : (
